051bee039d5fe4af6b0859a50a82ca90c177cf7f
matengda444 <matengda444@163.com>
2017-10-25 14:56:11 +0800

    update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -5,3 +5,14 @@
  * Date: 2017/10/25
  * Time: 14:51
  */
+$config = array(
+    'databases' => array(
+        'type' => 'mysql',
+        'host' => 'localhost',
+        'port' => '3306',
+        'user' => 'root',
+        'pass' => 'root',
+        'charset' => 'utf8',
+        'dbname' => 'pbs'
+    )
+);
