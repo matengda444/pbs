1972d8c855a8981257d1c77e9760a0f6691f9963
matengda444 <matengda444@163.com>
2017-11-06 11:48:43 +0800

    update Comm class(controll and model)

--- app/controller/backend/Comm.php
+++ app/controller/backend/Comm.php
@@ -10,11 +10,25 @@ namespace app\controller\backend;
 
 
 use core\Controller;
+use app\model\Comm as CommModel;
 
 class Comm extends Controller
 {
     public function index()
     {
-
+        $comms = CommModel::model()->getList();
+        //var_dump($comms);exit;
+        return $this->_loadHtml('comm/index', array(
+            'icomments' => $comms
+        ));
+    }
+    public function delete()
+    {
+        $id = $_GET['id'];
+        if (CommModel::model()->deleteById($id)) {
+            return $this->_redirect('删除成功', '?c=Comm&p=backend&a=index');
+        } else {
+            return $this->_redirect('删除失败', '?c=Comm&p=backend&a=index');
+        }
     }
 }
--- app/model/Comm.php
+++ app/model/Comm.php
@@ -8,13 +8,19 @@
 
 namespace app\model;
 
-
 use core\Model;
-
 class Comm extends Model
 {
     public function getTableName()
     {
-
+        return 'comm';
+    }
+    public function getList()
+    {
+        $sql = "SELECT comm.*, article.name AS article_name, user.username as user_name, co.content AS reply_content
+                FROM comm LEFT JOIN article ON comm.article_id = article.id
+                LEFT JOIN user ON comm.user_id = user.id
+                LEFT JOIN comm co ON comm.reply_id = co.id";
+        return $this->getAll($sql);
     }
 }
