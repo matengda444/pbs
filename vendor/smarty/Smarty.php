5ddcbe15fa96183aa32f5a2eef0849d8cb0704b1
matengda444 <matengda444@163.com>
2017-11-09 16:09:12 +0800

    add Smarty class

---
+++ vendor/smarty/Smarty.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sa
+ * Date: 2017/11/9
+ * Time: 16:05
+ */
+namespace vendor;
+
+return __DIR__ . '/smarty/Smarty.class.php';
+
+use Smarty as BaseSmary
+
+class Smarty extends BaseSmarty
+{
+
+}
